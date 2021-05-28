cc028115ee32031b4351c14463523e12fcc83b6e
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-05-28 12:08:38 +0800

    dashboard polish

--- dashboard/index.php
+++ dashboard/index.php
@@ -11,23 +11,25 @@
           <div class="row">
             <div class="col-5 col-md-4">
               <div class="icon-big text-center icon-warning">
-                <i class="nc-icon nc-globe text-warning"></i>
+                <i class="nc-icon nc-single-02 text-warning"></i>
               </div>
             </div>
             <div class="col-7 col-md-8">
               <div class="numbers">
-                <p class="card-category">Capacity</p>
-                <p class="card-title">150GB<p>
+                <p class="card-category">Profile Information</p>
+                <p class="card-title">3/5<p>
               </div>
             </div>
           </div>
         </div>
         <div class="card-footer ">
           <hr>
+          <a href="user.php">
           <div class="stats">
             <i class="fa fa-refresh"></i>
             Update Now
           </div>
+          </a>
         </div>
       </div>
     </div>
@@ -42,18 +44,20 @@
             </div>
             <div class="col-7 col-md-8">
               <div class="numbers">
-                <p class="card-category">Revenue</p>
-                <p class="card-title">$ 1,345<p>
+                <p class="card-category">USAC Social Community Users</p>
+                <p class="card-title">1,345 <p>
               </div>
             </div>
           </div>
         </div>
         <div class="card-footer ">
           <hr>
+          <a href="community">
           <div class="stats">
             <i class="fa fa-calendar-o"></i>
-            Last day
+            Go to Community
           </div>
+          </a>
         </div>
       </div>
     </div>
@@ -68,18 +72,20 @@
             </div>
             <div class="col-7 col-md-8">
               <div class="numbers">
-                <p class="card-category">Errors</p>
-                <p class="card-title">23<p>
+                <p class="card-category">USAC News Portal Articles</p>
+                <p class="card-title">23m<p>
               </div>
             </div>
           </div>
         </div>
         <div class="card-footer ">
           <hr>
+          <a href="news-portal">
           <div class="stats">
             <i class="fa fa-clock-o"></i>
-            In the last hour
+            Go to News Portal
           </div>
+          </a>
         </div>
       </div>
     </div>
@@ -94,82 +100,10 @@
             </div>
             <div class="col-7 col-md-8">
               <div class="numbers">
-                <p class="card-category">Followers</p>
+                <p class="card-category">Website Users</p>
                 <p class="card-title">+45K<p>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="card-footer ">
-          <hr>
-          <div class="stats">
-            <i class="fa fa-refresh"></i>
-            Update now
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-12">
-      <div class="card ">
-        <div class="card-header ">
-          <h5 class="card-title">Users Behavior</h5>
-          <p class="card-category">24 Hours performance</p>
-        </div>
-        <div class="card-body ">
-          <canvas id=chartHours width="400" height="100"></canvas>
-        </div>
-        <div class="card-footer ">
-          <hr>
-          <div class="stats">
-            <i class="fa fa-history"></i> Updated 3 minutes ago
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-4">
-      <div class="card ">
-        <div class="card-header ">
-          <h5 class="card-title">Email Statistics</h5>
-          <p class="card-category">Last Campaign Performance</p>
-        </div>
-        <div class="card-body ">
-          <canvas id="chartEmail"></canvas>
-        </div>
-        <div class="card-footer ">
-          <div class="legend">
-            <i class="fa fa-circle text-primary"></i> Opened
-            <i class="fa fa-circle text-warning"></i> Read
-            <i class="fa fa-circle text-danger"></i> Deleted
-            <i class="fa fa-circle text-gray"></i> Unopened
-          </div>
-          <hr>
-          <div class="stats">
-            <i class="fa fa-calendar"></i> Number of emails sent
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-8">
-      <div class="card card-chart">
-        <div class="card-header">
-          <h5 class="card-title">NASDAQ: AAPL</h5>
-          <p class="card-category">Line Chart with Points</p>
-        </div>
-        <div class="card-body">
-          <canvas id="speedChart" width="400" height="100"></canvas>
-        </div>
-        <div class="card-footer">
-          <div class="chart-legend">
-            <i class="fa fa-circle text-info"></i> Tesla Model S
-            <i class="fa fa-circle text-warning"></i> BMW 5 Series
-          </div>
-          <hr />
-          <div class="card-stats">
-            <i class="fa fa-check"></i> Data information certified
           </div>
         </div>
       </div>
--- dashboard/tables.php
+++ dashboard/tables.php
@@ -7,7 +7,17 @@ include "header.php";
     <div class="col-md-12">
       <div class="card">
         <div class="card-header">
-          <h4 class="card-title"> Simple Table</h4>
+          <h4 class="card-title"> User Table</h4>
+          <form onsubmit="return searchUser();">
+            <div class="input-group no-border">
+              <input type="text" value="" class="form-control" placeholder="Search...">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <i class="nc-icon nc-zoom-split"></i>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
         <div class="card-body">
           <div class="table-responsive">
@@ -131,133 +141,24 @@ include "header.php";
         </div>
       </div>
     </div>
+  </div>
+  <div class="row">
     <div class="col-md-12">
-      <div class="card card-plain">
-        <div class="card-header">
-          <h4 class="card-title"> Table on Plain Background</h4>
-          <p class="card-category"> Here is a subtitle for this table</p>
-        </div>
-        <div class="card-body">
-          <div class="table-responsive">
-            <table class="table">
-              <thead class=" text-primary">
-                <th>
-                  Name
-                </th>
-                <th>
-                  Country
-                </th>
-                <th>
-                  City
-                </th>
-                <th class="text-right">
-                  Salary
-                </th>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    Dakota Rice
-                  </td>
-                  <td>
-                    Niger
-                  </td>
-                  <td>
-                    Oud-Turnhout
-                  </td>
-                  <td class="text-right">
-                    $36,738
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    Minerva Hooper
-                  </td>
-                  <td>
-                    Curaçao
-                  </td>
-                  <td>
-                    Sinaai-Waas
-                  </td>
-                  <td class="text-right">
-                    $23,789
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    Sage Rodriguez
-                  </td>
-                  <td>
-                    Netherlands
-                  </td>
-                  <td>
-                    Baileux
-                  </td>
-                  <td class="text-right">
-                    $56,142
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    Philip Chaney
-                  </td>
-                  <td>
-                    Korea, South
-                  </td>
-                  <td>
-                    Overland Park
-                  </td>
-                  <td class="text-right">
-                    $38,735
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    Doris Greene
-                  </td>
-                  <td>
-                    Malawi
-                  </td>
-                  <td>
-                    Feldkirchen in Kärnten
-                  </td>
-                  <td class="text-right">
-                    $63,542
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    Mason Porter
-                  </td>
-                  <td>
-                    Chile
-                  </td>
-                  <td>
-                    Gloucester
-                  </td>
-                  <td class="text-right">
-                    $78,615
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    Jon Porter
-                  </td>
-                  <td>
-                    Portugal
-                  </td>
-                  <td>
-                    Gloucester
-                  </td>
-                  <td class="text-right">
-                    $98,615
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
+      <nav aria-label="...">
+        <ul class="pagination justify-content-center">
+          <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item active">
+            <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 </div>
