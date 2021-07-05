042a7cac4ec8bb1745ad7bf7d5201c4e6b49227d
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-07-05 18:09:25 +0800

    fresh perfect scroll code

--- dashboard/footer.php
+++ dashboard/footer.php
@@ -17,9 +17,13 @@
 <script src="../assets/vendor/jquery/jquery-3.3.1.min.js"></script>
 <script src="../assets/js/core/popper.min.js"></script>
 <script src="../assets/js/core/bootstrap.min.js"></script>
+<script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
 
 <!-- Jquery UI -->
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+  <!-- Control Center for Paper Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="../assets/js/paper-dashboard.js?v=2.0.0" type="text/javascript"></script>
 
 <script src="../assets/js/plugins/Bigpicture.js"></script>
 
