2ee4c9c1757535fba894fde34805f5a6e3757c91
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-07-07 19:51:06 +0800

    add mission statement to globals

--- cms_templates/cms_globals.php
+++ cms_templates/cms_globals.php
@@ -9,5 +9,10 @@
     </cms:editable>
     <cms:editable name='fb_page' label='Facebook page link' type='text' >https://www.facebook.com/usac/</cms:editable>
     <cms:editable name='twitter_page' label='Twitter page link' type='text' >https://www.twitter.com/usac/</cms:editable>
+    <cms:editable name='mission_statement' label='Mission Statement' desc='Enter Mission statement here' type='richtext'>
+    Realizing the full potential of the internet — universal access to research and education, full
+                    participation in culture — to drive a new era of development, growth, and productivity
+                   
+    </cms:editable>
 </cms:template>
 <?php COUCH::invoke(); ?>
