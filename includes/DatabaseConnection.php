c878eae37662ab4c45ab2408f4a6470a8a5ee7f8
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-07-09 08:44:16 +0800

    prevent showing msql errors

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -4,4 +4,4 @@ $dbname = getAppConfig('db_name');
 $user = getAppConfig('db_username');
 $password = getAppConfig('db_password');
 $pdo = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $password);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
