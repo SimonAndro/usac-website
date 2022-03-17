abb68d8b98310fef5e6e8b36ed8da978d0d04f58
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2022-03-17 23:22:56 +0800

    deploy to production

--- elections/voting/admin/includes/conn.php
+++ elections/voting/admin/includes/conn.php
@@ -1,6 +1,9 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
+	//$conn = new mysqli('localhost', 'root', '', 'votesystem');
 
+	$conn = new mysqli('localhost', 'timescom_2timesestore', '', 'timescom_votesystem');
+
+	
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
--- elections/voting/includes/conn.php
+++ elections/voting/includes/conn.php
@@ -1,5 +1,7 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
+	//$conn = new mysqli('localhost', 'root', '', 'votesystem');
+
+	$conn = new mysqli('localhost', 'timescom_2timesestore', '', 'timescom_votesystem');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
