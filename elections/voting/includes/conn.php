73b7324c43414b5db388913bf0ca3a34e28c85f2
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2022-03-17 23:31:53 +0800

    db pass

--- elections/voting/includes/conn.php
+++ elections/voting/includes/conn.php
@@ -1,7 +1,7 @@
 <?php
 	//$conn = new mysqli('localhost', 'root', '', 'votesystem');
 
-	$conn = new mysqli('localhost', 'timescom_2timesestore', '', 'timescom_votesystem');
+	$conn = new mysqli('localhost', 'timescom_2timesestore', 'X0QdrbN$Uqk+', 'timescom_votesystem');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
