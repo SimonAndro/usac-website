8fdbcf2f23ee32ce4691efed8ad492652ab8a64a
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2022-03-21 22:48:48 +0800

    total voters verified

--- elections/voting/index.php
+++ elections/voting/index.php
@@ -13,7 +13,7 @@
 <body class="hold-transition login-page">
 	<div class="login-box">
 		<div class="login-logo">
-			<b>Voting System</b>
+			<b>USAC ELECTIONS</b>
 		</div>
 
 		<!-- <div class="login-box-body">
